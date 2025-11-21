ec7cc6cd34aa52245873395a61462e38d5f75bf1
FICT1 <ficallejas@uamv.edu.ni>
2025-11-21 07:20:12 -0600

    Primer trabajo

---
+++ Practica/Practica/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Practica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCrear = new System.Windows.Forms.Button();
+            this.lblNumeroAleatorio = new System.Windows.Forms.Label();
+            this.tbBusqueda = new System.Windows.Forms.TextBox();
+            this.busqueda = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.Location = new System.Drawing.Point(34, 75);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(102, 69);
+            this.btnCrear.TabIndex = 0;
+            this.btnCrear.Text = "Crear";
+            this.btnCrear.UseVisualStyleBackColor = true;
+            this.btnCrear.Click += new System.EventHandler(this.btnCrear_Click);
+            // 
+            // lblNumeroAleatorio
+            // 
+            this.lblNumeroAleatorio.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblNumeroAleatorio.Location = new System.Drawing.Point(165, 75);
+            this.lblNumeroAleatorio.Name = "lblNumeroAleatorio";
+            this.lblNumeroAleatorio.Size = new System.Drawing.Size(123, 104);
+            this.lblNumeroAleatorio.TabIndex = 1;
+            this.lblNumeroAleatorio.Click += new System.EventHandler(this.lblNumeroAleatorio_Click);
+            // 
+            // tbBusqueda
+            // 
+            this.tbBusqueda.Location = new System.Drawing.Point(66, 276);
+            this.tbBusqueda.Name = "tbBusqueda";
+            this.tbBusqueda.Size = new System.Drawing.Size(222, 22);
+            this.tbBusqueda.TabIndex = 3;
+            this.tbBusqueda.TextChanged += new System.EventHandler(this.tbBusqueda_TextChanged);
+            // 
+            // busqueda
+            // 
+            this.busqueda.AutoSize = true;
+            this.busqueda.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.busqueda.Location = new System.Drawing.Point(100, 253);
+            this.busqueda.Name = "busqueda";
+            this.busqueda.Size = new System.Drawing.Size(150, 20);
+            this.busqueda.TabIndex = 4;
+            this.busqueda.Text = "Buscar Numeros";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(114, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Agregar Numeros";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.Controls.Add(this.btnBuscar);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.busqueda);
+            this.panel1.Controls.Add(this.tbBusqueda);
+            this.panel1.Controls.Add(this.lblNumeroAleatorio);
+            this.panel1.Controls.Add(this.btnCrear);
+            this.panel1.Location = new System.Drawing.Point(88, 32);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(341, 383);
+            this.panel1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(151, 188);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "presionar para recargar";
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(136, 304);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 28);
+            this.btnBuscar.TabIndex = 7;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCrear;
+        private System.Windows.Forms.Label lblNumeroAleatorio;
+        private System.Windows.Forms.TextBox tbBusqueda;
+        private System.Windows.Forms.Label busqueda;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnBuscar;
+    }
+}
+
---
+++ Practica/Practica/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Ejercicio 1: Búsqueda lineal en un arreglo
+//Crear un programa que:
+//• Genere un arreglo de 20 números enteros aleatorios.
+//• Solicite un número al usuario.
+//• Implemente una búsqueda lineal para determinar si el número existe en el arreglo.
+//• Muestre la posición o un mensaje indicando que no existe.
+
+namespace Practica
+{
+    
+    public partial class Form1 : Form
+    {
+        int[] arreglo = new int[20];
+        Random rand = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        
+
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+            
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                arreglo[i] = rand.Next(1, 101); 
+            }
+
+
+        }
+
+        private void lblNumeroAleatorio_Click(object sender, EventArgs e)
+        {
+           lblNumeroAleatorio.Text = string.Join(", ", arreglo);
+        }
+
+        private void tbBusqueda_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            int num = int.Parse(tbBusqueda.Text);
+            if (int.TryParse(tbBusqueda.Text, out int numer)
+)
+            {
+
+                int pos = -1;
+
+                for (int i = 0; i < arreglo.Length; i++)
+                {
+                    if (arreglo[i] == num)
+                    {
+                        pos = i;
+                        break;
+                    }
+                }
+
+                if (pos != -1)
+                {
+                    MessageBox.Show("El número está en la posición: " + pos);
+                    tbBusqueda.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("El número no existe en el arreglo.");
+                    tbBusqueda.Clear();
+                }
+
+
+            }
+        }
+    }
+}
